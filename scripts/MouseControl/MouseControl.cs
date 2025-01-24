9f5b71dc8864dfc10634f7de10c61e572dee3b56
matan2010 <matan203040@gmail.com>
2025-01-24 17:23:47 +0200

    reduce scale

--- scripts/MouseControl/MouseControl.cs
+++ scripts/MouseControl/MouseControl.cs
@@ -3,17 +3,40 @@ using System;
 
 public partial class MouseControl : Node2D
 {
+	private Vector2 originalScale;
+    private Vector2 originalPosition;
+	private float timer = 0f;
+	
+	private float scaleTime = 0.2f;
 	private float currentAngle = 0f;
+	private bool isScaling = false;
+    private float scaleDuration = 0.1f;
+    private float scaleTimer = 0f;
+
+
 		// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		originalScale = Scale;
+        originalPosition = Position;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Vector2 mousePosition = GetViewport().GetMousePosition();
+		if (isScaling)
+        {
+            timer += (float)delta;
 
+    
+            if (timer >= scaleTime)
+            {
+                Scale = originalScale; 
+                Position = originalPosition; 
+                isScaling = false;
+            }
+        }
+		Vector2 mousePosition = GetViewport().GetMousePosition();
 
         Vector2 direction = mousePosition - GlobalPosition;
 
@@ -26,8 +49,24 @@ public partial class MouseControl : Node2D
         {
             currentAngle = angle;
         }
-
-		
 		Rotation = currentAngle;
+		
+		
 	}
+
+	public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent)
+        {
+            if (mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+            {
+                GD.Print("Left mouse button clicked!");
+				Scale = new Vector2(Scale.X, Scale.Y * 0.8f);
+				Position = new Vector2(Position.X, Position.Y + 10);
+
+				isScaling = true;
+                timer = 0f;
+            }
+        }
+    }
 }
