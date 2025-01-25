e8912771ae7de6d9e9515f738fb11c22baa58cef
Rabbival <87331993+Rabbival@users.noreply.github.com>
2025-01-25 23:44:47 +0200

    add instruction

--- scripts/App.cs
+++ scripts/App.cs
@@ -11,7 +11,7 @@ public partial class App : Node2D
 	{
 		_gameWonLabel.Visible = false;
 		_bubblesContainer.GameWon += (int score) => {
-			_gameWonLabel.Text = "Game won!\nTurns taken: " + score;
+			_gameWonLabel.Text = "Game won!\nTurns taken: " + score  + "\n(Press any key)";
 			_gameWonLabel.Visible = true;
 		};
 		_bubblesContainer.GameRestarted += () => {
